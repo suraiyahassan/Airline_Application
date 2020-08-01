a6f1e33dbe890eaed9213974492fcad87662467f
Weroko <devops@weroko.com>
2020-08-02 01:22:42 +0530

    Authentication

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../service/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService: AuthenticationService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
+logOut(){
+  this.loginService.logOut();
+    this.router.navigate(['login']);
+}
 
 }
